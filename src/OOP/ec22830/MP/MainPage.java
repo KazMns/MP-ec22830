69ff0570b7c938f54ffdb01c8bf6f56c6d1d67f2
ec22830 <k.p.a.muniesa@se22.qmul.ac.uk>
2023-04-11 14:58:23 +0200

    test

--- src/OOP/ec22830/MP/MainPage.java
+++ src/OOP/ec22830/MP/MainPage.java
@@ -1,4 +1,25 @@
 package OOP.ec22830.MP;
 
+import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
+
+import javax.swing.*;
+
 public class MainPage {
+
+    private JComboBox comboBox1;
+    private JPanel panel1;
+
+    public MainPage(){
+        AutoCompleteDecorator.decorate(comboBox1);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        frame.setVisible(true);
+        frame.setSize(300,300);
+        frame.setContentPane(new MainPage().panel1);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.pack();
+    }
 }
